d7beabd1fa66b88f5cd2e5b3d40db8d0bd5346f0
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-01-13 03:18:22 +0300

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide() {
+    return 2000 / 100;
+  }
+  
+  console.log(divide());
+  
+  const square = (x) => x * x;
+  const add = (a, b) => a + b;
+  
+  module.exports = { multiply, divide, square, add };
+  
